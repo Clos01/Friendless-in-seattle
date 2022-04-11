7e19d54af71ff310194b532438175280cfc0d26b
Carlos Rivas <Cjrivas00@gmail.com>
2022-04-10 20:56:01 -0400

    comment table

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE user (
     user_name VARCHAR(30) NOT NULL,
     about VARCHAR(500) NOT NULL,
     location VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -16,6 +16,16 @@ CREATE TABLE posts (
     id INT NOT NULL ATUO_INCREMENT,
     user_id INT NOT NULL,
     post_content VARCHAR(1000) NOT NULL,
+    _created TIMESTAMP Default NOW(),
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Comments (
+    id INT ATUO_INCREMENT NOT NULL,
+    title VARCHAR(75) NOT NULL,
+    content TEXT NOT Null,
+    _created TIMESTAMP Default NOW(),
+    PRIMARY KEy (id),
+    FOREIGN KEY (Postsid) REFERNCES posts (id),
+
+)
