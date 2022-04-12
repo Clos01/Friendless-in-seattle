edad4e371a8c7b9a1f2aa3485ea2c0b5ec5efbf2
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-04-12 17:29:49 -0400

    modles

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,3 @@
-drop database if exists friendless-in-seattle;
-create database friendless-in-seattle;
+DROP DATABASE IF EXISTS friendless;
 
-
-CREATE TABLE user (
-    id INT NOT NULL ATUO_INCREMENT,
-    user_name VARCHAR(30) NOT NULL,
-    about VARCHAR(500) NOT NULL,
-    location VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE posts (
-    id INT NOT NULL ATUO_INCREMENT,
-    user_id INT NOT NULL,
-    post_content VARCHAR(1000) NOT NULL,
-    PRIMARY KEY (id)
-);
-
+CREATE DATABASE  friendless;
