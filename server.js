0fd012bb105c7b13fa95e8729002ced74fcde358
Sam Clark <86984214+Sam-Clark1@users.noreply.github.com>
2022-04-14 20:27:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const helmet = require('helmet');
 const PORT = process.env.PORT || 3333;
 const app = express();
 const db = require('./models');
-const sequelize = require('./config/config')
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
@@ -56,8 +55,6 @@ db.sequelize.sync(syncOptions).then(() => {
   if (app.get('env') !== 'test' && syncOptions.force) {
     require('./db/seed')(db);
   }
-
-checkdb();
 
   app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
